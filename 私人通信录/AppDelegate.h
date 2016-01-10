f059549da3a00378df7a41750bd48550b661528d
zhanglu1988em <zhanglu1988em163.com@zhanglu1988emdemacbook-pro.local>
2016-01-10 22:13:27 +0800

    Initial Commit

---
+++ 私人通信录/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  私人通信录
+//
+//  Created by zhanglu1988em on 16/1/10.
+//  Copyright © 2016年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 私人通信录/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  私人通信录
+//
+//  Created by zhanglu1988em on 16/1/10.
+//  Copyright © 2016年 wbder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
